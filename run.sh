ff79978ce1c35ec4fa9e28a991fecf36104bd582
Igor Leonovich <email@domain.com>
2021-07-31 23:19:19 +0300

    DRS-13.2.1: Basic Setup: Environments: Development: Update run.sh

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 SCRIPT_PATH=$(realpath $0)
 DOORS_SERVER_MONOLITH_PATH=$(dirname $SCRIPT_PATH)
-docker compose -f $DOORS_SERVER_MONOLITH_PATH/docker-compose-dev.yml up -d
+docker compose -f $DOORS_SERVER_MONOLITH_PATH/docker-compose-dev.yml --env-file $DOORS_SERVER_MONOLITH_PATH/.env.development up -d
