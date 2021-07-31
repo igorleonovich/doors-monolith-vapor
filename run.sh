9a6df0e4407f275aa1ae677302f7307d06171c25
Igor Leonovich <email@domain.com>
2021-07-31 23:39:10 +0300

    DRS-13.2.1: Basic Setup: Environments: Development: run.sh: Add dev/prod argument

--- run.sh
+++ run.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
 
+DEPLOYMENT_ENVIRONMENT="development"
+if [ $# -eq 0 ]
+  then
+    echo "No arguments supplied"
+  else
+    case "$1" in
+      dev) DEPLOYMENT_ENVIRONMENT="development"
+          ;;
+      prod) DEPLOYMENT_ENVIRONMENT="production"
+          ;;
+    esac
+fi
 SCRIPT_PATH=$(realpath $0)
 DOORS_SERVER_MONOLITH_PATH=$(dirname $SCRIPT_PATH)
-docker compose -f $DOORS_SERVER_MONOLITH_PATH/docker-compose-dev.yml --env-file $DOORS_SERVER_MONOLITH_PATH/.env.development up -d
+docker compose -f ${DOORS_SERVER_MONOLITH_PATH}/docker-compose.${DEPLOYMENT_ENVIRONMENT}.yml --env-file ${DOORS_SERVER_MONOLITH_PATH}/.env.${DEPLOYMENT_ENVIRONMENT} up -d
